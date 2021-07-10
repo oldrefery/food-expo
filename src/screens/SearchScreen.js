6c9a1967662c830515cbd5d9029165773adeb9d8
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-10 18:25:18 +0300

    feat: Add stack navigator and template of main screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, Text, View} from "react-native";
+
+const SearchScreen = () => {
+    return <View><Text>Search screen</Text></View>
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
