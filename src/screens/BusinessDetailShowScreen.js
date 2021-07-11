d3049c0862840550eb759e8febf2429435d770b5
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-11 20:48:20 +0300

    feat: Add fetch data from yelp API by business id and handling errors

--- src/screens/BusinessDetailShowScreen.js
+++ src/screens/BusinessDetailShowScreen.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import yelp from '../api/yelp';
 
 const BusinessDetailShowScreen = ({ navigation }) => {
   const id = navigation.getParam('id');
+  const [businessDetail, setBusinessDetail] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    getBusinessDetail(id);
+  }, []);
+
+  const getBusinessDetail = (id) => {
+    yelp
+      .get(`/${id}`)
+      .then((res) => setBusinessDetail(res.data))
+      .catch((e) => setErrorMessage(e.message));
+  };
 
   return (
     <View>
-      <Text>{id} Business Detail Show Screen</Text>
+      <Text>Business Detail Show Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      {businessDetail && <Text>{businessDetail.name}</Text>}
     </View>
   );
 };
